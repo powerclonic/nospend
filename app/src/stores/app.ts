7de11020640179dbf82348714efe3f663da386ca
Matheus Dresch <matheus.dresch02@gmail.com>
2024-08-29 16:57:57 -0300

    feat: add notification functionality to the frontend

--- app/src/stores/app.ts
+++ app/src/stores/app.ts
@@ -1,8 +1,21 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import type { MessageType } from "@/types";
 
-export const useAppStore = defineStore('app', {
+export const useAppStore = defineStore("app", {
   state: () => ({
-    //
+    message: "",
+    type: "" as MessageType,
+    _timeout: 0,
   }),
-})
+  actions: {
+    setMessage(message: string, type: MessageType) {
+      clearTimeout(this._timeout);
+
+      this.message = message;
+      this.type = type;
+
+      this._timeout = setTimeout(() => (this.message = ""), 5000);
+    },
+  },
+});
